a59780edf8fa31ca40987f46228dea1a4c955a6b
Prashanth <spprashaant@gmail.com>
2019-02-13 12:31:32 +0530

    Initial Commit

---
+++ Features/Feature1/Feature1.feature
@@ -0,0 +1,12 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="2410969b-6355-4389-88a6-d0d997c5b6d2" description="SharePoint Add-in Feature" featureId="2410969b-6355-4389-88a6-d0d997c5b6d2" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="REST-Simple-Read Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="0922f65c-ebd4-4c48-a1e3-bd9efe196cb3" />
+    <projectItemReference itemId="e03108bf-4e59-4c78-a862-df31d0db6174" />
+    <projectItemReference itemId="f3a9b867-88a1-4338-be94-1895955f1c24" />
+    <projectItemReference itemId="dc124af5-e4ec-4e80-95ad-559fb25ec8f5" />
+    <projectItemReference itemId="0d813862-051c-4c15-b371-e1bc6b2541a7" />
+    <projectItemReference itemId="40475498-9f80-4469-87d4-b3259d659ce7" />
+    <projectItemReference itemId="e3ef0d0c-de34-4b3e-a3a4-7e7e9758f384" />
+  </projectItems>
+</feature>
---
+++ pkg/Debug/REST_Simple_Read/REST-Simple-Read_Feature1/Scripts/App.js
@@ -0,0 +1,159 @@
+﻿'use strict';
+
+ExecuteOrDelayUntilScriptLoaded(initializePage, "sp.js");
+
+function initializePage() {
+    //#region Common Code
+    function readAjaxCall(restUrl) {
+        var dfd = $.ajax({
+            url: restUrl,
+            type: "GET",
+            contentType: "application/json;odata=verbose",
+            headers: {
+                "accept": "application/json;odata=verbose"
+            }
+        });
+        return dfd.promise();
+    }
+    function displayError(error) {
+        $('#message').append("<br/> There was an error getting the data..");
+        $('#message').append(error);
+    }
+    //#endregion
+
+    // This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+    $(document).ready(function () {
+        $("#readLists").click(readLists);
+        $("#readListItems").click(readListItems);
+        $("#readItemsPaged").click(readListItemsPaged);
+        
+    });
+
+    //#region Lists
+    function readLists() {
+        var msg = "Getting all lists...";
+        $('#message').text(msg);
+        var restUrl = SP.Utilities.UrlBuilder.urlCombine(_spPageContextInfo.webServerRelativeUrl,
+            "_api/web/lists");
+        var readPromise = readAjaxCall(restUrl);
+        readPromise.then(
+            function (data, status, jqXHR) {
+                displayLists(data);
+            },
+            function (jqXHR, status, error) {
+                displayError(error);
+            }
+        );
+    }
+    function displayLists(data) {
+        var msg = "<br/><br/><b>Lists on this site are:</b><ul>";
+        if (data.d.results.length > 0) {
+            $.each(data.d.results, function (index, value) {
+                msg += "<li>" + value.Title + "</li>";
+            });
+        }
+        else {
+            msg += "<li>" + data.d.Title + "</li>";
+        }
+        msg += "</ul>";
+        $('#message').append(msg);
+    }
+    //#endregion
+
+    //#region List Items
+    function readListItems() {
+        var msg = "Getting all lists items...";
+        var listName = "SampleData";
+        $('#message').text(msg);
+        var restUrl = SP.Utilities.UrlBuilder.urlCombine(_spPageContextInfo.webServerRelativeUrl,
+            "_api/web/lists/GetByTitle('" + listName + "')/items");
+        var readPromise = readAjaxCall(restUrl);
+        readPromise.then(
+            function (data, status, jqXHR) {
+                displayListItems(data);
+            },
+            function (jqXHR, status, error) {
+                displayError(error);
+            }
+        );
+    }
+
+    function displayListItems(data) {
+        var msg = "<br/><br/><b>List Items are:</b><ul>";
+        if (data.d.results.length) {
+            if (data.d.results.length > 1) {
+                msg += data.d.results.length + " items:<ul>";
+                $.each(data.d.results, function (index, value) {
+                    msg += "<li>" + value.Title + "</li>";
+                });
+
+            }
+            else {
+                msg += "1 item:<ul>";
+                msg += "<li>" + data.d.results[0].Title + "</li>";
+            }
+            msg += "</ul>";
+        }
+        else {
+            msg += "no items";
+        }
+
+        if (data.d.__next) {
+            msg += "<br />";
+            msg += "Results are paged - next set of results available here: <br />" + data.d.__next;
+        }
+        $('#message').append(msg);
+    }
+    //#endregion
+
+    //#region Paging List Items
+    function readListItemsPaged() {
+        var msg = "Getting next page lists items...";
+        var listName = "SampleData";
+        var pageNum = 0;
+        var pageSize = 3;
+        $('#message').text(msg);
+        var restUrl = SP.Utilities.UrlBuilder.urlCombine(_spPageContextInfo.webServerRelativeUrl,
+            "_api/web/lists/GetByTitle('" + listName + "')/items?$top=" + pageSize);
+        //need to add skiptoken unencoded or else the % in %26 and %3d gets encoded to %25 which breaks things
+        restUrl = encodeURI(restUrl) + "&$skiptoken=Paged%3dTRUE%26p_ID%3d" + pageNum * pageSize;
+
+        var readPromise = readAjaxCall(restUrl);
+        readPromise.then(
+            function (data, status, jqXHR) {
+                displayListItems(data);
+            },
+            function (jqXHR, status, error) {
+                displayError(error);
+            }
+        );
+    }
+
+    function displayListItemsPaged(data) {
+        var msg = "<br/><br/><b>List Items are:</b><ul>";
+        if (data.d.results.length) {
+            if (data.d.results.length > 1) {
+                msg += data.d.results.length + " items:<ul>";
+                $.each(data.d.results, function (index, value) {
+                    msg += "<li>" + value.Title + "</li>";
+                });
+
+            }
+            else {
+                msg += "1 item:<ul>";
+                msg += "<li>" + data.d.results[0].Title + "</li>";
+            }
+            msg += "</ul>";
+        }
+        else {
+            msg += "no items";
+        }
+
+        if (data.d.__next) {
+            msg += "<br />";
+            msg += "Results are paged - next set of results available here: <br />" + data.d.__next;
+        }
+        $('#message').append(msg);
+    }
+    //#endregion
+}
